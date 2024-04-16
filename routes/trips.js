4f53aff53511fd57b24d053d80b1c56c9c3b4d7e
GregoireMeillet <meillet.gregoire@gmail.com>
2024-04-16 13:51:40 +0200

    route /trips

---
+++ routes/trips.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+const moment = require("moment");
+
+const fetch = require("node-fetch");
+const Trip = require("../models/trips");
+
+router.get("/:departure/:arrival/:date", async (req, res) => {
+  const timestamp = req.params.date;
+  const date = moment.unix(Number(timestamp)).format("YYYY-MM-DD");
+
+  const allTrips = await Trip.find();
+  const matchTrip = allTrips.filter(
+    (trips) =>
+      trips.departure.toLowerCase() === req.params.departure.toLowerCase() &&
+      trips.arrival.toLowerCase() === req.params.arrival.toLowerCase() &&
+      moment(trips.date).format("YYYY-MM-DD") === date
+  );
+  if (matchTrip != "") {
+    // If at least 1 trip found
+    res.json({ result: true, allTrips: matchTrip }); // Send all the trips found
+  } else {
+    // If no trips found
+    res.json({ result: false, allTrips: "No trips found" });
+  }
+});
+
+module.exports = router;
