ef97826eead43165d17190b45278415dcdcff79e
GregoireMeillet <meillet.gregoire@gmail.com>
2024-04-17 09:17:45 +0200

    add cors in app.js

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const bookingRouter = require("./routes/bookings");
 
 var app = express();
 
+const cors = require("cors");
+app.use(cors());
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
